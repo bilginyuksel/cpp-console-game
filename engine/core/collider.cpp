38d1ef00a87e29aa6386989e86ce0629ae9cde20
bilginyuksel <bilgin.yuksel96@gmail.com>
2020-12-29 23:57:24 +0300

    rect creation for every bit

--- engine/core/collider.cpp
+++ engine/core/collider.cpp
@@ -10,10 +10,20 @@ void Collider :: SetCollision(Collision* collision) {
 Rect* Collider :: GetRect() {
     return rect_;
 }
+Rect Collider ::GetRectR() {
+    int left = this->center_->GetX() - 5;
+    int right = this->center_->GetX() + 5;
+    int top = this->center_->GetY() - 5;
+    int bottom = this->center_->GetY() + 5;
+    return Rect(left, right, bottom, top);
+}
 Collider :: Collider(int left, int right, int bottom, int top){
     this->rect_ = new Rect(left, right, bottom, top);
     // this->collision_ = new Collision; Default collision function
 }
 Collider :: Collider(Rect *rect) {
     this->rect_ = rect;
+}
+Collider::Collider(Position *pos) {
+    this->center_ = pos;
 }
